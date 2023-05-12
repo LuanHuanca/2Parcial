c048f2b2ffbbf5eaae5aeaacfa7e5ff37973ee1b
Luis Huanca <luanhuancam@gmail.com>
2023-05-12 11:28:48 -0400

    primer componente

---
+++ src/componentes/PrimerComponente.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './PrimerComponente.css'
+export const PrimerComponente = () => {
+  return (
+    <div className='Target'>
+        <div className='figure'>
+            <span></span>
+        </div>
+        <h1>Crunchy Butterscotch</h1>
+        <h2>Butterscotch & Cashews</h2>
+        <h3>Bs 18</h3>
+        <button>Add</button>
+    </div>
+  )
+}
+
